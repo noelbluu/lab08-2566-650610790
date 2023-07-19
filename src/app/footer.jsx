cb534919232a03ab036d851196c7cf265a04aa0f
૮꒰ྀི つ˘ `⸝⸝ ꒱ྀིა   ♡ <110964402+noelbluu@users.noreply.github.com>
2023-07-19 11:58:05 +0700

    Delete footer.jsx

--- src/app/footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = (props) => {
-    return (
-        <div className="mt-3">
-            <p className="text-secondary text-center">
-                copyright © 2023 Panida Suthapakti 650610790
-            </p>
-        </div>
-    );
-};
