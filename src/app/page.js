ed0230aae4634d37ed8a5c8967eb6655d7ae59e8
૮꒰ྀི つ˘ `⸝⸝ ꒱ྀིა   ♡ <110964402+noelbluu@users.noreply.github.com>
2023-07-19 11:59:46 +0700

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,30 @@
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header'
+import { Task } from '@/components/Task';
+import { TaskInput } from '@/components/TaskInput'
+
+export default function Home() {
+  return (
+    // Main container
+    <div className="container mx-auto">
+      {/* header section */}
+      <Header />
+
+      {/* tasks container */}
+      <div style={{ maxWidth: "400px" }} className="mx-auto">
+        {/* task input */}
+        <TaskInput />
+        {/* task */}
+        <Task title="Read a book" />
+        {/* task */}
+        <Task title="Take a shower" />
+        {/* task */}
+        <Task title="Sleep" />
+      </div>
+
+      {/* //footer section */}
+      <Footer />
+
+    </div>
+  );
+}
